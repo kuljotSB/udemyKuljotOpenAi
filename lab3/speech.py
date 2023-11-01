8cc6d3b643f1c9b67f5ad4468452c4d8313ec7fa
kuljotSB <kuljot.bakshi302005@gmail.com>
2023-11-02 01:46:47 +0530

    added few more lines of code

---
+++ lab3/speech.py
@@ -0,0 +1,8 @@
+#importing important libraries and utilites for azure openAi model
+import os
+import openai
+import json
+import requests
+
+#importing important libraries and utilities for azure speech service SDK
+import azure.cognitiveservices.speech as speech_sdk
