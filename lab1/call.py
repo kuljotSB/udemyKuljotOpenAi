71e6304c6543c39d6953631e0214b66bc05567b0
kuljotSB <147621230+kuljotSB@users.noreply.github.com>
2023-12-08 17:07:19 +0530

    updated API calling syntax

--- lab1/call.py
+++ lab1/call.py
@@ -1,23 +1,21 @@
 #importing importing libraries
 import os
-import openai
+from openai import AzureOpenAI
 from dotenv import load_dotenv
 
-#setting openAI configurations
-load_dotenv()
-openai.api_type = "azure"
-openai.api_base = os.getenv('OPENAI_API_BASE')
-openai.api_version = "2023-03-15-preview"
-openai.api_key = os.getenv('OPENAI_API_KEY')
+#creating an azure openAi client
+client = AzureOpenAI(
+  azure_endpoint = os.getenv("OPENAI_API_BASE"), 
+  api_key=os.getenv("OPENAI_API_KEY"),  
+  api_version="2023-05-15"
+)
 
-#Send request to Azure OpenAI model
-response = openai.ChatCompletion.create(
-    engine="YOUR_ENGINE_NAME_HERE",
-    temperature = 0.7,
-    max_tokens=120,
+#send request to Azure OpenAi model
+response = client.chat.completions.create(
+    model="gpt-35-turbo", # model = "deployment_name".
     messages=[
-        {"role":"system", "content":"Your are an assistant. Your work is to provide people with relevant and factual information"},
-        {"role":"user", "content":"list out all the players in the Indian National Cricket Team"}
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "list out all the players in the indian national cricket team?"}
     ]
 )
 
