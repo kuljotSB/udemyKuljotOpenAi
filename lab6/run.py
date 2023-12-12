ce0c3acdbf6165b5643aa169d400f9be2a9b86ac
kuljotSB <147621230+kuljotSB@users.noreply.github.com>
2023-12-13 01:25:50 +0530

    Create run.py

---
+++ lab6/run.py
@@ -0,0 +1,46 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+# Add Azure OpenAI package
+import openai
+from openai import AzureOpenAI
+
+
+def main(): 
+        
+    try: 
+    
+        
+        
+        # Read text from file
+        text = open(file=r"C:\Users\HP VICTUS\Downloads\test_folder\udemyKuljotOpenAi\lab6\text_file.txt", encoding="utf8").read()
+        
+        print("\nSending request for summary to Azure OpenAI endpoint...\n\n")
+        
+        # Add code to build request...
+        client = AzureOpenAI(
+            azure_endpoint="YOUR_AZURE_ENDPOINT_HERE",
+            api_key="YOUR_AZURE_OPENAI_KEY_HERE",
+            api_version="2023-05-15"
+        )
+        
+        messages = [
+            {"role":"system", "content":"you are an assistant that helps summarise text"},
+            {"role":"user", "content":text}
+        ]
+        
+        response = client.chat.completions.create(
+            model = "YOUR_MODEL_NAME_GOES_HERE",
+            temperature=0.7,
+            messages=messages
+        )
+        
+        print(response.choices[0].message.content)
+        
+
+    except Exception as ex:
+        print(ex)
+
+if __name__ == '__main__': 
+    main()
