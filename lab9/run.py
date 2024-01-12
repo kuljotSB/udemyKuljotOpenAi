f8c3011c07d533285808b6b2d85224d783c94e74
kuljotSB <147621230+kuljotSB@users.noreply.github.com>
2024-01-12 14:46:40 +0530

    Create run.py

---
+++ lab9/run.py
@@ -0,0 +1,37 @@
+import os
+import json
+import requests
+import openai
+from dotenv import load_dotenv
+
+load_dotenv()
+url=os.getenv("URL")
+api_key=os.getenv("API_KEY")
+
+
+headers={
+    "Content-Type":"application/json",
+    "api-key": api_key
+}
+
+body={
+    "messages": [
+        {"role":"system", "content":"You are a helpful assistant."},
+        {"role":"user","content":[{
+            "type":"text",
+            "text":"Describe this picture"
+        },
+                                  {
+                                      "type":"image_url",
+                                      "image_url":{
+                                          "url":"https://learn.microsoft.com/azure/ai-services/computer-vision/media/quickstarts/presentation.png",
+                                          "detail":"high"
+                                      }
+                                  }]}
+    ]
+}
+
+response=requests.post(url,headers=headers,json=body).json()
+caption=response['choices'][0]['message']['content']
+print(caption)
+
