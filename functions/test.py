8f71190acd0b1e6abd4272434a55743bdcf8c3bb
kuljot singh bakshi <147621230+kuljotSB@users.noreply.github.com>
2024-04-30 11:26:10 +0530

    Update test.py

--- functions/test.py
+++ functions/test.py
@@ -5,16 +5,17 @@ from openai import AzureOpenAI
 from dotenv import load_dotenv
 import os
 
+def main():
 #creating an Azure OpenAI client
-load_dotenv()
-client = AzureOpenAI(
+ load_dotenv()
+ client = AzureOpenAI(
   azure_endpoint = os.getenv("oai_base"), 
   api_key=os.getenv("oai_key"),  
   api_version="2024-02-15-preview"
-)
+ )
 
 
-functions=[
+ functions=[
         {
             "name":"getWeather",
             "description":"Retrieve real-time weather information/data about a particular location/place",
@@ -32,37 +33,47 @@ functions=[
         }
     ] 
 
-messages=[
+ messages=[
     {"role":"system", "content":"you are an assistant that helps people retrieve real-time weather data/info"},
     {"role":"user", "content":"how is the weather in Mumbai?"}
     
-]
+ ]
 
-initial_response = client.chat.completions.create(
-    model="YOUR_MODEL_NAME", # model = "deployment_name".
+ initial_response = client.chat.completions.create(
+    model=os.getenv('model_name'), # model = "deployment_name".
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "How is the weather in Mumbai?"}
     ],
    functions=functions
-)
+ )
+
+ function_name = initial_response.choices[0].message.function_call.name
+ function_argument = json.loads(initial_response.choices[0].message.function_call.arguments)
+ location= function_argument['location']
+ if(location):
+    print(location)
+    get_weather(location)
+
+ 
 
 
-function_name = initial_response.choices[0].message.function_call.name
-function_argument = json.loads(initial_response.choices[0].message.function_call.arguments)
-location= function_argument['location']
-print(location)
-#calling open weather map API for information retrieval
-#fetching latitude and longitude of the specific location respectively
-url = "http://api.openweathermap.org/geo/1.0/direct?q=" + location + "&limit=1&appid=YOUR_API_KEY"
-response=requests.get(url)
-get_response=response.json()
-latitude=get_response[0]['lat']
-longitude = get_response[0]['lon']
 
-url_final = "https://api.openweathermap.org/data/2.5/weather?lat=" + str(latitude) + "&lon=" + str(longitude) + "&appid=YOUR_API_KEY"
-final_response = requests.get(url_final)
-final_response_json = final_response.json()
-weather=final_response_json['weather'][0]['description']
-print(weather)
 
+def get_weather(location):
+   #calling open weather map API for information retrieval
+   #fetching latitude and longitude of the specific location respectively
+    url = "http://api.openweathermap.org/geo/1.0/direct?q=" + location + "&limit=1&appid=YOUR_API_KEY"
+    response=requests.get(url)
+    get_response=response.json()
+    latitude=get_response[0]['lat']
+    longitude = get_response[0]['lon']
+
+    url_final = "https://api.openweathermap.org/data/2.5/weather?lat=" + str(latitude) + "&lon=" + str(longitude) + "&appid=YOUR_API_KEY"
+    final_response = requests.get(url_final)
+    final_response_json = final_response.json()
+    weather=final_response_json['weather'][0]['description']
+    print(weather)
+
+if __name__ == "__main__":
+    main()
